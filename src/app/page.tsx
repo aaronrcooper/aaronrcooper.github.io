df49ff689e8e61bc006395896e57b00dd61adc8b
Aaron Cooper <mr.aaroncooper@gmail.com>
2024-03-17 21:16:09 -0400

    MDX page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         Aaron Cooper is a software engineer based in Pittsburgh, PA
       </div>
-    </main>
+    </div>
   );
 }
